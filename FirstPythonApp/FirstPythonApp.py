eb412613b9996eb8c1928c39ae850b9ef6d2bbfc
Sean Holtshousen <holtshousens@gmail.com>
2016-01-15 21:09:53 +0000

    cleaned up code

--- FirstPythonApp/FirstPythonApp.py
+++ FirstPythonApp/FirstPythonApp.py
@@ -1,35 +1,6 @@
-import os
-
-class myClass:
-    def myClass(aName):
-        name = aName
-
-    def myFunction(name):
-        print("This is " + name + "'s function")
-
-    def getDirectoryContents(path):
-        for root, dirs, files in os.walk(path):
-            for name in files:
-                if name.endswith((".py")):
-                    print(name)
-
-    def getDirectoryContentsPrint(path):
-        pythonFiles = [os.path.join(root, name)
-                for root, dirs, files in os.walk(path)
-                    for name in files 
-                        if name.endswith((".py"))]
-        print(pythonFiles)
-
-
-
-
-#myClass.myFunction("Sean")
-
-#myClass.getDirectoryContents("./")
-
-#myClass.getDirectoryContentsPrint("./")
 import pypyodbc 
 
+# Read config parameters
 from xml.dom import minidom
 xmldoc = minidom.parse('./config.xml')
 configdriver = xmldoc.getElementsByTagName('driver')
@@ -39,17 +10,16 @@ server = configserver[0].attributes['name'].value
 configdatabase = xmldoc.getElementsByTagName('database')
 database = configdatabase[0].attributes['name'].value
 
-
+# connect to sql server
 connection = pypyodbc.connect('Driver=' + driver + 'Server=' + server + 'Database=' + database) 
-
 cursor = connection.cursor() 
 
+# execute a script
 SQLCommand = ("SELECT Name FROM dbo.Merchants") 
-
 cursor.execute(SQLCommand) 
 
+#fetch results and display to console
 results = cursor.fetchone() 
-
 while results:
      print(str(results[0]))
      results = cursor.fetchone() 
