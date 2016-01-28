c121e2c4af04a4cd548dc904285c408e5abaa688
Sean Holtshousen <holtshousens@gmail.com>
2016-01-28 10:27:17 +0000

    wrapping solution in a class

--- FirstPythonApp/FirstPythonApp.py
+++ FirstPythonApp/FirstPythonApp.py
@@ -1,49 +1,53 @@
-import pyodbc 
+import pyodbc
 
 # Read config parameters
-
 from xml.dom import minidom
 from pyodbc import ProgrammingError
 
-def extractConnectionString():
-    xmldoc = minidom.parse('./config.xml')
-    configdriver = xmldoc.getElementsByTagName('driver')
-    driver = configdriver[0].attributes['name'].value
-    configserver = xmldoc.getElementsByTagName('server')
-    server = configserver[0].attributes['name'].value
-    configdatabase = xmldoc.getElementsByTagName('database')
-    database = configdatabase[0].attributes['name'].value 
-    return driver, server, database 
+class ConnectToSQL:
+    def __init__(self,servertype):
+        self.st = servertype
 
-# connect to sql server
-def connectToDB(driver, server, database):
-    try:
-        connection = pypyodbc.connect('Driver=' + driver + 'Server=' + server + 'Database=' + database) 
-    except ProgrammingError as error:
-        print("Programming Error: ", error)
-    else:
-        cursor = connection.cursor() 
-        print("*** Connection to " + server + " Established ***")
-        return cursor, connection
+    def extractConnectionString():
+        xmldoc = minidom.parse('./config.xml')
+        configdriver = xmldoc.getElementsByTagName('driver')
+        driver = configdriver[0].attributes['name'].value
+        configserver = xmldoc.getElementsByTagName('server')
+        server = configserver[0].attributes['name'].value
+        configdatabase = xmldoc.getElementsByTagName('database')
+        database = configdatabase[0].attributes['name'].value
+        return driver, server, database
 
-# return top 100 rows
-def returnTop100(cursor, schemaname, tablename, fieldname):
-    SQLCommand = ("SELECT TOP 100 " + fieldname + " FROM " + schemaname + "." + tablename) 
-    print("*** Executing script: " + SQLCommand + " ***")
-    cursor.execute(SQLCommand) 
-    #fetch results and display to console
-    print("*** Printing Result START ***")
-    results = cursor.fetchone() 
-    while results:
-         print(str(results[0]))
-         results = cursor.fetchone() 
-    print("*** Printing Result END ***")
+    # connect to sql server
+    def connectToDB(driver, server, database):
+        try:
+            connection = pyodbc.connect('Driver=' + driver + 'Server=' + server + 'Database=' + database)
+        except ProgrammingError as error:
+            print("Programming Error: ", error)
+        else:
+            cursor = connection.cursor()
+            print("*** Connection to " + server + " Established ***")
+            return cursor, connection
 
-def disconnectFromDB(con):
-    con.close()
-    print("*** Disconnection made ***")
+    # return top 100 rows
+    def returnTop100(cursor, schemaname, tablename, fieldname):
+        SQLCommand = ("SELECT TOP 100 " + fieldname + " FROM " + schemaname + "." + tablename)
+        print("*** Executing script: " + SQLCommand + " ***")
+        cursor.execute(SQLCommand)
+        # fetch results and display to console
+        print("*** Printing Result START ***")
+        results = cursor.fetchone()
+        while results:
+            print(str(results[0]))
+            results = cursor.fetchone()
+        print("*** Printing Result END ***")
 
-myDriver, myServer, myDatabase = extractConnectionString()
-myCursor, myConnection = connectToDB(myDriver, myServer, myDatabase)
-returnTop100(myCursor, "dbo", "Merchants", "Name")
-disconnectFromDB(myConnection)
+    def disconnectFromDB(con):
+        con.close()
+        print("*** Disconnection made ***")
+
+# How to use the class methods
+myDriver, myServer, myDatabase = ConnectToSQL.extractConnectionString()
+myCursor, myConnection = ConnectToSQL.connectToDB(myDriver, myServer, myDatabase)
+ConnectToSQL.returnTop100(myCursor, "dbo", "Merchants", "Name")
+ConnectToSQL.disconnectFromDB(myConnection)
