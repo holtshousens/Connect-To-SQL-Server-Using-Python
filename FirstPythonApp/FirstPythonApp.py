9bd87747afaa1954ccbffc4c28ac4558282a4158
Sean Holtshousen <holtshousens@gmail.com>
2016-01-28 19:53:46 +0000

    standardising code and cleaning it up

--- FirstPythonApp/FirstPythonApp.py
+++ FirstPythonApp/FirstPythonApp.py
@@ -4,11 +4,13 @@ import pyodbc
 from xml.dom import minidom
 from pyodbc import ProgrammingError
 
-class ConnectToSQL:
-    def __init__(self,servertype):
+
+class ConnectToDB:
+    def __init__(self, servertype):
         self.st = servertype
 
-    def extractConnectionString():
+    @staticmethod
+    def connectionstring():
         xmldoc = minidom.parse('./config.xml')
         configdriver = xmldoc.getElementsByTagName('driver')
         driver = configdriver[0].attributes['name'].value
@@ -19,7 +21,8 @@ class ConnectToSQL:
         return driver, server, database
 
     # connect to sql server
-    def connectToDB(driver, server, database):
+    @staticmethod
+    def connect(driver, server, database):
         try:
             connection = pyodbc.connect('Driver=' + driver + 'Server=' + server + 'Database=' + database)
         except ProgrammingError as error:
@@ -30,10 +33,11 @@ class ConnectToSQL:
             return cursor, connection
 
     # return top 100 rows
-    def returnTop100(cursor, schemaname, tablename, fieldname):
-        SQLCommand = ("SELECT TOP 100 " + fieldname + " FROM " + schemaname + "." + tablename)
-        print("*** Executing script: " + SQLCommand + " ***")
-        cursor.execute(SQLCommand)
+    @staticmethod
+    def getsample(cursor, schemaname, tablename, fieldname):
+        script = ("SELECT TOP 100 " + fieldname + " FROM " + schemaname + "." + tablename)
+        print("*** Executing script: " + script + " ***")
+        cursor.execute(script)
         # fetch results and display to console
         print("*** Printing Result START ***")
         results = cursor.fetchone()
@@ -42,12 +46,13 @@ class ConnectToSQL:
             results = cursor.fetchone()
         print("*** Printing Result END ***")
 
-    def disconnectFromDB(con):
-        con.close()
-        print("*** Disconnection made ***")
+    @staticmethod
+    def disconnect(connection):
+        connection.close()
+        print('*** Disconnection made ***')
 
 # How to use the class methods
-myDriver, myServer, myDatabase = ConnectToSQL.extractConnectionString()
-myCursor, myConnection = ConnectToSQL.connectToDB(myDriver, myServer, myDatabase)
-ConnectToSQL.returnTop100(myCursor, "dbo", "Merchants", "Name")
-ConnectToSQL.disconnectFromDB(myConnection)
+myDriver, myServer, myDatabase = ConnectToDB.connectionstring()
+myCursor, myConnection = ConnectToDB.connect(driver=myDriver, server=myServer, database=myDatabase)
+ConnectToDB.getsample(cursor=myCursor, schemaname="dbo", tablename="Merchants", fieldname="Name")
+ConnectToDB.disconnect(connection=myConnection)
